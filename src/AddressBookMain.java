0701e81db2a12028b6be4423f2436e49cb682f6e
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-16 10:20:16 +0530

    [Vaibhav] added ability to create contacts

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
 
 public class AddressBookMain
 {
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String city;
+	public String state;
+	public int zip;
+	public int phoneNumber;
+	public String email;
+	public int i=1;
+	public AddressBookMain()
+	{
+		
+	}
+	public AddressBookMain(String firstName,String lastName,String address,String city,String state,int zip,int phoneNumber,String email)
+	{
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.address=address;
+		this.city=city;
+		this.state=state;
+		this.zip=zip;
+		this.phoneNumber=phoneNumber;
+		this.email=email;
+	}
+	
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcome to Address Book System !!!");
+	
 	}
 
 }
